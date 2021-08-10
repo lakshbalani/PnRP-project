6cb30c50b46c4ae638a7aa4a2f05527370c411f1
Laksh Balani <laksh.balani@students.iiit.ac.in>
2021-08-10 23:56:28 +0530

    Update GBM_Stock_Predictor.m

--- Code/GBM_Stock_Predictor.m
+++ Code/GBM_Stock_Predictor.m
@@ -13,13 +13,13 @@ sigma_1 = 0.5; % defines the volatility of the stock.
 sigma_2 = 0.1; % defines the volatility of the stock.
 sigma_3 = 0.01; % defines the volatility of the stock.
 
-Z = zeros(1,n);% defines an array of n zeros, which will contain the cumulative sum of change in stock price.
+Z = zeros(1,n);% lines 16-18 defines an array of n zeros, which will contain the cumulative sum of change in stock price.
 X = zeros(1,n);
 Y = zeros(1,n);
 ds_1 = mu*s_0*dt+sigma_1*s_0*dz; % calculates the change in value of the stock based on the formula used in the Geometric Brownian Model.
 ds_2 = mu*s_0*dt+sigma_2*s_0*dz;
 ds_3 = mu*s_0*dt+sigma_3*s_0*dz;
-Z(1) = ds_1(1); % lines 16-19 contain the cumulative sum of the change in value of the stock over every trading day.
+Z(1) = ds_1(1); % lines 22-29 contain the cumulative sum of the change in value of the stock over every trading day.
 X(1) = ds_2(1);
 Y(1) = ds_3(1);
 for i = 2:n
@@ -28,11 +28,11 @@ for i = 2:n
     Y(i) = Y(i-1) + ds_3(i);
 end
 
-st_1 = s_0 + Z; % st_1 shows the value of the stock after 't_grid' days of trading
+st_1 = s_0 + Z; % st_1, st_2 and st_3 shows the value of the stock after 't_grid' days of trading 
 st_2 = s_0 + X; 
 st_3 = s_0 + Y; 
 
-figure(1); % plots the future values of the stock based on given parameters of sigma, mu, ds and dz as used in the equation on line 
+figure(1); % plots the future values of the stock based on given parameters of sigma, mu, ds and dz as used in the equation on line 19-21
 plot(t_grid,[s_0,st_1],'-r');
 hold on;
 plot(t_grid,[s_0,st_2],'-g');
